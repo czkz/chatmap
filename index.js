3955be109351d9f43780c2f60f608fa3bfee5223
Dmitry <deekovalev@gmail.com>
2022-07-03 21:35:56 +0300

    First resemblance of the map

--- index.js
+++ index.js
@@ -1,92 +1,109 @@
-var dom = document.getElementById('chart-container');
-var myChart = echarts.init(dom, null, {
-    renderer: 'canvas',
-    useDirtyRect: false
-});
-var app = {};
-var option;
+'use strict';
 
-var dataCount = 0;
-var CHUNK_COUNT = 1;
-// https://blog.openstreetmap.org/2012/04/01/bulk-gps-point-data/
-function fetchData(idx) {
-    if (idx >= CHUNK_COUNT) {
-        return;
+const CSVToArray = (data, delimiter = ',', omitFirstRow = false) => data
+    .slice(omitFirstRow ? data.indexOf('\n') + 1 : 0)
+    .split('\n')
+    .map(v => v.split(delimiter));
+
+function convertData(inp) {
+    const ret = [];
+    for (const [cityName, viewers] of Object.entries(inp)) {
+        const cityInfo = cities[cityName];
+        ret.push([cityInfo.lng, cityInfo.lat, cityName, viewers]);
     }
-    var dataURL = 'data.bin';
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', dataURL, true);
-    xhr.responseType = 'arraybuffer';
-    xhr.onload = function (e) {
-        var rawData = new Int32Array(this.response);
-        var data = new Float32Array(rawData.length);
-        var addedDataCount = rawData.length / 2;
-        for (var i = 0; i < rawData.length; i += 2) {
-            data[i] = rawData[i + 1] / 1e7;
-            data[i + 1] = rawData[i] / 1e7;
+    return ret;
+}
+
+(async function() {
+
+    const csv = await fetch('cities/worldcities_clean.csv')
+        .then(resp => resp.text());
+    const data = CSVToArray(csv, ',', true);
+    const cities = Object.fromEntries(data.map(line => [
+        line[0],
+        {
+            lat: line[1],
+            lng: line[2],
+            country: line[3],
+            population: line[4],
         }
+    ]));
+    window.cities = cities;
+
+    const myChart = echarts.init(
+        document.getElementById('chart-container'),
+        null,
+        {
+            renderer: 'canvas',
+            useDirtyRect: false
+        }
+    );
+
+    window.rawData = Object.create(null);
+
+    window.addPoint = function(cityName) {
+        const info = cities[cityName];
+        if (info == undefined) {
+            console.log('Undefined city!');
+            return;
+        }
+
+        if (rawData[cityName] == undefined) {
+            rawData[cityName] = 0;
+        }
+        rawData[cityName] += 1;
+
         myChart.appendData({
             seriesIndex: 0,
-            data: data
+            data: convertData(rawData)
         });
-        fetchData(idx + 1);
     };
-    xhr.send();
-}
-option = {
-    backgroundColor: '#000',
-    title: {
-        text: '10000000 GPS Points',
-        left: 'center',
-        textStyle: {
-            color: '#fff'
-        }
-    },
-    geo: {
-        map: 'world',
-        roam: true,
-        label: {
-            emphasis: {
-                show: false
+
+    const option = {
+        tooltip: {},
+        backgroundColor: '#000',
+        title: {
+            text: '10000000 GPS Points',
+            left: 'center',
+            textStyle: {
+                color: '#fff'
             }
         },
-        silent: true,
-        itemStyle: {
-            normal: {
-                areaColor: '#323c48',
-                borderColor: '#111'
-            },
-            emphasis: {
-                areaColor: '#2a333d'
-            }
-        }
-    },
-    series: [
-        {
-            name: 'Messages',
-            type: 'scatterGL',
-            progressive: 1e6,
-            coordinateSystem: 'geo',
-            symbolSize: 10,
-            zoomScale: 0.002,
-            blendMode: 'lighter',
-            large: true,
-            itemStyle: {
-                color: 'rgb(20, 15, 2)'
-            },
-            postEffect: {
-                enable: true
+        geo: {
+            map: 'world',
+            roam: true,
+            label: {
+                emphasis: {
+                    show: false
+                }
             },
             silent: true,
-            dimensions: ['lng', 'lat'],
-            data: new Float32Array()
+            itemStyle: {
+                normal: {
+                    areaColor: '#323c48',
+                    borderColor: '#111'
+                },
+                emphasis: {
+                    areaColor: '#2a333d'
+                }
+            }
+        },
+        series: {
+            type: 'effectScatter',
+            coordinateSystem: 'geo',
+            geoIndex: 0,
+            symbolSize: function (params) {
+                return params[3] + 5;
+            },
+            itemStyle: {
+                color: '#F6F6F6'
+            },
+            data: []
         }
-    ]
-};
-fetchData(0);
-
-if (option && typeof option === 'object') {
+    };
     myChart.setOption(option);
-}
 
-window.addEventListener('resize', myChart.resize);
+    addPoint('Moscow');
+
+    window.addEventListener('resize', myChart.resize);
+})();
